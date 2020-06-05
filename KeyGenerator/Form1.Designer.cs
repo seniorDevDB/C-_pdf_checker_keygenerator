4635e5963714510e93089c8bc4d146e8fb441205
seniorDevDB <dbupworks@gmail.com>
2020-06-05 11:24:28 -0500

    first commit

---
+++ KeyGenerator/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace KeyGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.hardwareId = new System.Windows.Forms.TextBox();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.codeLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input The Code";
+            // 
+            // hardwareId
+            // 
+            this.hardwareId.Location = new System.Drawing.Point(201, 146);
+            this.hardwareId.Name = "hardwareId";
+            this.hardwareId.Size = new System.Drawing.Size(311, 20);
+            this.hardwareId.TabIndex = 1;
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateBtn.Location = new System.Drawing.Point(216, 228);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(153, 42);
+            this.generateBtn.TabIndex = 2;
+            this.generateBtn.Text = "Generate";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
+            // 
+            // codeLabel
+            // 
+            this.codeLabel.AutoSize = true;
+            this.codeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codeLabel.Location = new System.Drawing.Point(257, 51);
+            this.codeLabel.Name = "codeLabel";
+            this.codeLabel.Size = new System.Drawing.Size(0, 20);
+            this.codeLabel.TabIndex = 3;
+            this.codeLabel.UseWaitCursor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.generateBtn);
+            this.panel1.Controls.Add(this.codeLabel);
+            this.panel1.Controls.Add(this.hardwareId);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(110, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(566, 330);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Code Generator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox hardwareId;
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.Label codeLabel;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ KeyGenerator/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyGenerator
+{
+    public partial class Form1 : Form
+    {
+        public int sum;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateBtn_Click(object sender, EventArgs e)
+        {
+            string hardwareID = hardwareId.Text;
+            for (int i = 0; i < hardwareID.Length; i++)
+            {
+                sum += (int)hardwareID[i];
+            }
+            Console.WriteLine(sum);
+            codeLabel.Text = sum.ToString();
+            sum = 0;
+        }
+    }
+}
